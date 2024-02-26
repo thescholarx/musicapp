09a47d7f0ce9444983cb52b17a60aaced5ca3cc4
Devraj <115987350+thescholarx@users.noreply.github.com>
2024-02-26 12:52:24 +0530

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -3,6 +3,50 @@ const playlist = [
         id: 1,
         title: "playlist 1",
         mood: "Happy",
+        image: "images/music.jpg",
+        creator:"playlist creator"
+    },
+    {
+        id: 2,
+        title: "playlist 2",
+        mood: "Chill",
+        image: "images/music.jpg",
+        creator:"playlist creator"
+    },
+    {
+        id: 3,
+        title: "playlist 3",
+        mood: "Relaxing",
+        image: "images/music.jpg",
+        creator:"playlist creator"
+    },
+    {
+        id: 4,
+        title: "playlist 4",
+        mood: "Energetic",
+        image: "images/music.jpg",
+        creator:"playlist creator"
+    },
+    {
+        id: 5,
+        title: "playlist 5",
+        mood: "Sad",
+        image: "images/music.jpg",
+        creator:"playlist creator"
+    },
+    {
+        id: 6,
+        title: "playlist 6",
+        mood: "Happy",
+        image: "images/music.jpg",
+        creator:"playlist creator"
+    }
+];
+const playlist2 = [
+    {
+        id: 1,
+        title: "playlist 1",
+        mood: "Happy",
         image: "images/music2.jpg",
         creator:"playlist creator"
     },
@@ -43,4 +87,5 @@ const playlist = [
     }
 ];
 
-export default playlist;
+
+export { playlist, playlist2 };
